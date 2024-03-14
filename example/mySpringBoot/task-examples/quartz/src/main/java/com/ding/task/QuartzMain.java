cfe0db76b528f28e4a3066509f70a1f9047b9086
tintin <fyx912@163.com>
2024-03-14 20:57:07 +0800

    update task-examples

---
+++ example/mySpringBoot/task-examples/quartz/src/main/java/com/ding/task/QuartzMain.java
@@ -0,0 +1,13 @@
+package com.ding.task;
+
+/**
+ * <p> ${descitption} </p>
+ *
+ * @author Tintin
+ * @date 2024-03-14 20:19:29
+ **/
+public class QuartzMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
